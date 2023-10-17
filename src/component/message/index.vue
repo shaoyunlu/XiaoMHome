c0c3d55e8bc2b53210be3d2288653fc821d42fbe
shaoyunlu <shaoyunlu@126.com>
2023-10-17 14:06:26 +0800

    Popover 组件 功能

--- src/component/message/index.vue
+++ src/component/message/index.vue
@@ -3,7 +3,7 @@
     <p>常用于主动操作后的反馈提示。 与 Notification 的区别是后者更多用于系统级通知的被动提醒。</p>
     <h2>基础用法</h2>
     <p>从顶部出现，3 秒后自动消失。</p>
-    <xmv-code>
+    <xmv-code :code="code_1">
         <xmv-button @click="open">Show message</xmv-button>
     </xmv-code>
     <h2>不同状态</h2>
@@ -12,7 +12,7 @@
         <p text="sm">当需要自定义更多属性时，<code>Message</code> 也可以接收一个对象为参数。 
             比如，设置 <code>type</code> 字段可以定义不同的状态，默认为<code>info。</code></p>
     </p>
-    <xmv-code>
+    <xmv-code :code="code_2">
         <xmv-button @click="open1('success')">success</xmv-button>
         <xmv-button @click="open1('warning')">warning</xmv-button>
         <xmv-button @click="open1('info')">message</xmv-button>
@@ -24,7 +24,7 @@
         和 Notification 一样，Message 拥有可控的 <code>duration</code>， 默认的关闭时间为 3000 毫秒，
         当把这个属性的值设置为<code>0</code>便表示该消息不会被自动关闭。</p>
     </p>
-    <xmv-code>
+    <xmv-code :code="code_3">
         <xmv-button @click="open2('success')">success</xmv-button>
         <xmv-button @click="open2('warning')">warning</xmv-button>
         <xmv-button @click="open2('info')">message</xmv-button>
@@ -32,7 +32,7 @@
     </xmv-code>
     <h2>分组消息合并</h2>
     <p text="sm"><p>设置 <code>grouping</code> 为 true，内容相同的 <code>message</code> 将被合并。</p></p>
-    <xmv-code>
+    <xmv-code :code="code_4">
         <xmv-button @click="open3('success')">success</xmv-button>
         <xmv-button @click="open3('warning')">warning</xmv-button>
         <xmv-button @click="open3('info')">message</xmv-button>
@@ -77,7 +77,66 @@ export default defineComponent({
             })
         }
 
-        return {open,open1,open2,open3}
+        const code_1 =
+                        `
+                            // JS
+                            const open = ()=>{
+                                XmvMessage({
+                                    message : '这是一条消息提示。'
+                                })
+                            }
+                            // Template
+                            <xmv-button @click="open">Show message</xmv-button>
+                        `
+        const code_2 =
+                        `
+                            // JS
+                            const open1 = (type)=>{
+                            XmvMessage({
+                                    message : '这是一条消息提示。',
+                                    type : type
+                                })
+                            }
+                            // Template
+                            <xmv-button @click="open1('success')">success</xmv-button>
+                            <xmv-button @click="open1('warning')">warning</xmv-button>
+                            <xmv-button @click="open1('info')">message</xmv-button>
+                            <xmv-button @click="open1('error')">error</xmv-button>
+                        `
+        const code_3 =
+                        `
+                            // JS
+                            const open2 = (type)=>{
+                                XmvMessage({
+                                    message : '这是一条消息提示。',
+                                    type : type,
+                                    showClose: true
+                                })
+                            }
+                            // Template
+                            <xmv-button @click="open2('success')">success</xmv-button>
+                            <xmv-button @click="open2('warning')">warning</xmv-button>
+                            <xmv-button @click="open2('info')">message</xmv-button>
+                            <xmv-button @click="open2('error')">error</xmv-button>
+                        `
+        const code_4 =
+                        `
+                            // JS
+                            const open3 = (type)=>{
+                                XmvMessage({
+                                    message : '这是一条消息提示。' + type,
+                                    type : type,
+                                    showClose: true,
+                                    grouping: true
+                                })
+                            }
+                            // Template
+                            <xmv-button @click="open3('success')">success</xmv-button>
+                            <xmv-button @click="open3('warning')">warning</xmv-button>
+                            <xmv-button @click="open3('info')">message</xmv-button>
+                            <xmv-button @click="open3('error')">error</xmv-button>
+                        `
+        return {open,open1,open2,open3,code_1,code_2,code_3,code_4}
     }
 })
 </script>
--- src/component/popover/index.vue
+++ src/component/popover/index.vue
@@ -1,14 +1,178 @@
 <template>
     <h1>Popover 气泡卡片</h1>
     <h2>基础用法</h2>
+    <p>与Tooltip相似，Popover也是基于<code>XmvPopper</code>的构建。 因此对于重复属性，请参考 Tooltip 的文档，在此文档中不做详尽解释。</p>
+    <p text="sm">
+        <p>
+            <code>placement</code> 属性决定 popover  的弹出位置，值为：<code>top</code>、<code>bottom</code>、<code>left</code>、<code>right</code>。
+            <br/>
+            <code>trigger</code> 属性被用来决定 popover 的触发方式，支持的触发方式： <code>hover</code>、<code>click</code>。
+        </p>
+    </p>
+    <xmv-code :code="code_1">
+        <xmv-popover
+            trigger="hover">
+            <template #trigger>
+                <xmv-button class="m-2">Hover to activate</xmv-button>
+            </template>
+            <div style="padding:10px">this is content, this is content, this is content</div>
+        </xmv-popover>
+
+        <xmv-popover
+            trigger="click" placement="top">
+            <template #trigger>
+                <xmv-button class="m-2">Click to activate</xmv-button>
+            </template>
+            <div style="padding:10px">this is content, this is content, this is content</div>
+        </xmv-popover>
+    </xmv-code>
+    <h2>内容可扩展</h2>
+    <p>可以在 Popover 中嵌套其它组件。</p>
+    <p><p text="sm">以下为嵌套表格的例子。</p></p>
+    <xmv-code :code="code_2">
+        <xmv-popover  trigger="click" placement="right">
+            <template #trigger>
+                <xmv-button style="margin-right: 16px">Click to activate</xmv-button>
+            </template>
+            <div>
+                <xmv-table :data="gridData">
+                    <xmv-table-column width="150" prop="date" label="date" />
+                    <xmv-table-column width="100" prop="name" label="name" />
+                    <xmv-table-column width="300" prop="address" label="address" />
+                </xmv-table>
+            </div>
+        </xmv-popover>
+    </xmv-code>
+    <h2>关闭控制</h2>
+    <p>通过提供的 <code>hide</code> 方法，可进行手动关闭。</p>
+    <xmv-code :code="code_3">
+        <xmv-popover placement="top" ref="popoverRef">
+            <div style="padding:20px">
+                <p>Are you sure to delete this?</p>
+                <div style="text-align: right; margin: 0">
+                    <xmv-button size="small" text @click="visible = false">cancel</xmv-button>
+                    <xmv-button size="small" type="primary" @click="handleConfirm">confirm</xmv-button>
+                </div>
+            </div>
+            <template #trigger>
+                <xmv-button @click="visible = true">Delete</xmv-button>
+            </template>
+        </xmv-popover>
+    </xmv-code>
 </template>
 
 <script>
-import {defineComponent} from 'vue'
+import {defineComponent,ref} from 'vue'
 export default defineComponent({
     name:"",
     setup(props ,context) {
-        return {}
+        const popoverRef = ref(null)
+        const gridData = [
+                                {
+                                    date: '2016-05-02',
+                                    name: 'Jack',
+                                    address: 'New York City',
+                                },
+                                {
+                                    date: '2016-05-04',
+                                    name: 'Jack',
+                                    address: 'New York City',
+                                },
+                                {
+                                    date: '2016-05-01',
+                                    name: 'Jack',
+                                    address: 'New York City',
+                                },
+                                {
+                                    date: '2016-05-03',
+                                    name: 'Jack',
+                                    address: 'New York City',
+                                }
+                        ]
+        const handleConfirm = ()=>{
+            popoverRef.value.hide()
+        }
+
+        const code_1 =
+                        `
+                            <xmv-popover
+                                trigger="hover">
+                                <template #trigger>
+                                    <xmv-button class="m-2">Hover to activate</xmv-button>
+                                </template>
+                                <div style="padding:10px">this is content, this is content, this is content</div>
+                            </xmv-popover>
+
+                            <xmv-popover
+                                trigger="click" placement="top">
+                                <template #trigger>
+                                    <xmv-button class="m-2">Click to activate</xmv-button>
+                                </template>
+                                <div style="padding:10px">this is content, this is content, this is content</div>
+                            </xmv-popover>
+                        `
+        const code_2 =
+                        `
+                            // JS
+                            const gridData = [
+                                                        {
+                                                            date: '2016-05-02',
+                                                            name: 'Jack',
+                                                            address: 'New York City',
+                                                        },
+                                                        {
+                                                            date: '2016-05-04',
+                                                            name: 'Jack',
+                                                            address: 'New York City',
+                                                        },
+                                                        {
+                                                            date: '2016-05-01',
+                                                            name: 'Jack',
+                                                            address: 'New York City',
+                                                        },
+                                                        {
+                                                            date: '2016-05-03',
+                                                            name: 'Jack',
+                                                            address: 'New York City',
+                                                        }
+                                                ]
+                            // Template
+                            <xmv-popover  trigger="click" placement="right">
+                                <template #trigger>
+                                    <xmv-button style="margin-right: 16px">Click to activate</xmv-button>
+                                </template>
+                                <div>
+                                    <xmv-table :data="gridData">
+                                        <xmv-table-column width="150" prop="date" label="date" />
+                                        <xmv-table-column width="100" prop="name" label="name" />
+                                        <xmv-table-column width="300" prop="address" label="address" />
+                                    </xmv-table>
+                                </div>
+                            </xmv-popover>
+                        `
+        const code_3 =
+                        `
+                            // JS
+                            const popoverRef = ref(null)
+                            const handleConfirm = ()=>{
+                                popoverRef.value.hide()
+                            }
+                            // Template
+                            <xmv-popover placement="top" ref="popoverRef">
+                                <div style="padding:20px">
+                                    <p>Are you sure to delete this?</p>
+                                    <div style="text-align: right; margin: 0">
+                                        <xmv-button size="small" text @click="visible = false">cancel</xmv-button>
+                                        <xmv-button size="small" type="primary" @click="handleConfirm">confirm</xmv-button>
+                                    </div>
+                                </div>
+                                <template #trigger>
+                                    <xmv-button @click="visible = true">Delete</xmv-button>
+                                </template>
+                            </xmv-popover>
+                        `
+        return {gridData,popoverRef,handleConfirm,
+                code_1,code_2,code_3}
     }
 })
 </script>
