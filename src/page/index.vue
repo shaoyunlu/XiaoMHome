3f6665510a57c84bd7894df14d8ba7bcb9dca03d
shaoyunlu <shaoyunlu@126.com>
2023-10-25 15:54:55 +0800

    Release version

--- src/page/index.vue
+++ src/page/index.vue
@@ -1,6 +1,13 @@
 <template>
     <h1>Xiaomeng-Vue-UI</h1>
     <p>一个基于 vue3 的前端 UI 框架。</p>
+    <h2>安装方式</h2>
+    <pre style="font-size: 14px;margin: 0;">
+        <code class="language-javascript" 
+            style="display: block;margin-left: -200px;margin-top: -60px;margin-bottom: -80px;">
+                {{ code }}
+        </code>
+    </pre>
 </template>
 
 <script>
@@ -8,7 +15,11 @@ import {defineComponent} from 'vue'
 export default defineComponent({
     name:"",
     setup(props ,context) {
-        return {}
+        const code = 
+                        `
+                            npm install xiaomeng-vue-ui -S
+                        `
+        return {code}
     }
 })
 </script>
